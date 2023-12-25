65c9711ab5b05a714d3acd370181306b55c2c78c
Lewin Bormann <lbo@spheniscida.de>
2023-12-25 14:35:51 +0100

    Prove compatibility with Sequence.t

--- lib/Td.ml
+++ lib/Td.ml
@@ -2,6 +2,11 @@ open Base
 
 let fold_list f x l = List.fold_right ~init:x ~f l
 
+let rec fold_seq f x s =
+  match Sequence.hd s with
+  | None -> x
+  | Some h -> f h (fold_seq f x (Sequence.tl_eagerly_exn s))
+
 (* step function type *)
 type ('e, 'a) step = 'e -> 'a -> 'a
 type ('e, 'ee, 'a) td = ('ee, 'a) step -> ('e, 'a) step
@@ -14,21 +19,23 @@ let mapping (f : 'e -> 'ee) (step : ('ee, 'a) step) (x : 'e) (a : 'a) =
 let filtering (p : 'e -> bool) (step : ('e, 'a) step) (x : 'e) (a : 'a) =
   if p x then step x a else a
 
-let concatting (inner : ('ce, 'e, 'a) td) (step : ('e, 'a) step) (x : 'ce)
-    (a : 'a) =
-  inner step x a
-
 let apply f x = f x
 
 let compose (td1 : ('e, 'ee, 'a) td) (td2 : ('ee, 'eee, 'a) td)
-    (step : ('eee, 'a) step) (x : 'e) (a : 'a) =
-  (td1 (td2 step)) x a
+    (step : ('eee, 'a) step) : ('e, 'a) step =
+  td1 (td2 step)
 
 let run_list (td : ('e, 'ee, 'a) td) (l : 'e list) =
   let step = nothing (fun x xs -> x :: xs) in
   fold_list (td step) [] l
 
-let op1 =
+let run_seq (td : ('e, 'ee, 'a) td) (s : 'e Sequence.t) =
+  let step = nothing (fun x xs -> Sequence.append (Sequence.singleton x) xs) in
+  fold_seq (td step) Sequence.empty s
+
+(* test *)
+
+let op1 () =
   let filter_odd = filtering (fun x -> Int.(x % 2 = 1)) in
   let map_sq = mapping (fun x -> x * x) in
   let td = compose filter_odd map_sq in
@@ -38,5 +45,12 @@ let%test "test_op1" =
   let eq = Int.equal in
   let equal = List.equal eq in
   let inp = [ 1; 2; 3; 4; 5; 6 ] in
-  let outp = run_list op1 inp in
+  let outp = run_list (op1 ()) inp in
   equal outp [ 1; 9; 25 ]
+
+let%test "test_op1_sequence" =
+  let eq = Int.equal in
+  let equal = List.equal eq in
+  let inp = Sequence.of_list [ 1; 2; 3; 4; 5; 6 ] in
+  let outp = run_seq (op1 ()) inp in
+  equal (Sequence.to_list outp) [ 1; 9; 25 ]
