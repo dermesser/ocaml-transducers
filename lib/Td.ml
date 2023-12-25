f4024db6d26875a57a31f6c19252a094cdb7d1b7
Lewin Bormann <lbo@spheniscida.de>
2023-12-25 15:14:02 +0100

    Clean-up: better ergonomics for compose, run_

--- lib/Td.ml
+++ lib/Td.ml
@@ -13,35 +13,31 @@ type ('e, 'a) step = 'e -> 'a -> 'a
 type ('e, 'ee, 'a) td = ('ee, 'a) step -> ('e, 'a) step
 
 (* transducer constructors *)
-let nothing (step : ('e, 'a) step) (x : 'e) (a : 'a) = step x a
-
 let mapping (f : 'e -> 'ee) (step : ('ee, 'a) step) (x : 'e) (a : 'a) =
   step (f x) a
 
 let filtering (p : 'e -> bool) (step : ('e, 'a) step) (x : 'e) (a : 'a) =
   if p x then step x a else a
 
-let concatting (step : ('e, 'a) step) (x : 'e list) (a : 'a) =
+let lconcatting (step : ('e, 'a) step) (x : 'e list) (a : 'a) =
   fold_list step a x
 
 (* transducer combinators *)
 
-let apply f x = f x
-
-let compose (td2 : ('ee, 'eee, 'a) td) (td1 : ('e, 'ee, 'a) td)
+let compose (td1 : ('ee, 'eee, 'a) td) (td2 : ('e, 'ee, 'a) td)
     (step : ('eee, 'a) step) : ('e, 'a) step =
-  td1 (td2 step)
+  td2 (td1 step)
 
 (* transducer executors *)
 
 let run_list (td : ('e, 'ee, 'a) td) (l : 'e list) =
   let conj x xs = x :: xs in
-  let step = nothing conj in
+  let step = conj in
   fold_list (td step) [] l
 
 let run_seq (td : ('e, 'ee, 'a) td) (s : 'e Sequence.t) =
   let conj x xs = Sequence.append (Sequence.singleton x) xs in
-  let step = nothing conj in
+  let step = conj in
   fold_seq (td step) Sequence.empty s
 
 (* test *)
@@ -55,7 +51,7 @@ let op1 () =
 let op2 () =
   let map_plus3 = mapping (fun x -> x + 3) in
   let map_sq : (int, int list, _) td = mapping (fun x -> [ x; x * x ]) in
-  let concat_all : (int list, int, _) td = concatting in
+  let concat_all : (int list, int, _) td = lconcatting in
   let td = compose concat_all (compose map_sq map_plus3) in
   td
 
