17202dda8a12b60b226db88ee3d149d6a05f12ac
Lewin Bormann <lbo@spheniscida.de>
2023-12-27 23:49:36 +0100

    Add some minor comments

--- lib/Gen_reducer.ml
+++ lib/Gen_reducer.ml
@@ -1,6 +1,15 @@
 type ('a, 'e, 'r) red = { step : 'a -> 'e -> 'a; state : 'a; result : 'a -> 'r }
 type ('e, 'r) t = Red : ('a, 'e, 'r) red -> ('e, 'r) t
 
+(* Constructors *)
+
+(* A reducer is a stateful function which can be called repeatedly
+   on elements using the `reduce` function. The result
+   can be extracted using the `extract` function.
+
+   A red is typically wrapped in a `Red` constructor to hide the
+   state type of the reducer.
+*)
 let red step state result = Red { step; state; result }
 let make red = Red red
 
@@ -13,6 +22,9 @@ let extract (Red { result; state; _ }) = result state
 let map f (Red ({ result; _ } as prev)) =
   Red { prev with result = (fun state -> f (result state)) }
 
+let return x =
+  Red { step = (fun () _ -> ()); state = (); result = (fun () -> x) }
+
 let compose (Red { step = step1; state = state1; result = result1 })
     (Red { step = step2; state = state2; result = result2 }) =
   let step' (st1, st2) e =
@@ -24,10 +36,14 @@ let compose (Red { step = step1; state = state1; result = result1 })
   let result' (st1, _) = result1 st1 in
   Red { step = step'; state = state'; result = result' }
 
+(* Reducers *)
+
+(* Accumulates a list of stepped elements in reverse order *)
 let list_reducer () =
   let state = [] and result = List.rev and step xs x = x :: xs in
   red step state result
 
+(* Ignores all elements stepped through *)
 let noop_reducer () : (_, unit) t =
   let state = () and result _ = () and step _ _ = () in
   red step state result
